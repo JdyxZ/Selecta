aebd8f3253e33f79f6039c6d332ef4be65ebf8ad
JdyxZ <javier.serrano04@estudiant.upf.edu>
2023-03-29 16:13:16 +0200

    yt-dl core module

--- server/testing/tests.js
+++ server/testing/tests.js
@@ -6,6 +6,7 @@ const YOUTUBE = require("../utils/youtube.js");
 const fs = require('fs/promises');
 const {isArray} = require("../../public/framework/javascript.js");
 const {Song} = require('../model/model.js');
+const ytdl = require('ytdl-core');
 
 async function test()
 {
@@ -13,7 +14,14 @@ async function test()
     await DATABASE.init();
     await YOUTUBE.init();
 
-    await YOUTUBE.getVideosInfo("hola");
+    // await YOUTUBE.getVideosInfo("hola");
+    await fetchVideoStream("ntCZjb_AAWE")
+}
+
+async function fetchVideoStream(videoID)
+{
+    const response = await ytdl.getInfo("ntCZjb_AAWE");
+    console.log(response);
 }
 
 test();
